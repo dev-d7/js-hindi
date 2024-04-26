3029b98b7b2266cdfe8d0a82708312ea43a7436c
dev-d7 <167406376+dev-d7@users.noreply.github.com>
2024-04-26 06:24:25 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = undefined
+
+console.log (typeof score)
+console.log (typeof (score));
+
+let ValueinNum = Number(score);
+
+console.log (typeof (ValueinNum));
+console.log(ValueinNum);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 false -> 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedin  = Boolean(isLoggedIn);
+console.log(booleanisLoggedin);
+
+// 1 -> true 0 -> false
+// "" -> false
+// "devansh" -> true
