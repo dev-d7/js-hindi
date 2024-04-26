23f2573115bb5159d208435b6a87540e4db3343f
dev-d7 <167406376+dev-d7@users.noreply.github.com>
2024-04-26 11:23:18 +0000

    datatypes_summary

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -21,3 +21,35 @@ console.log(booleanisLoggedin);
 // 1 -> true 0 -> false
 // "" -> false
 // "devansh" -> true
+
+// **************************  Operations ************************************
+
+let value = 3;
+let negvalue = -value;
+
+console.log(negvalue);
+
+let str = "hello"
+let name1 = " Devansh"
+
+let final = str + name1
+console.log(final)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+
