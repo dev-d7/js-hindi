84b89113ea167e186fc2fba23926d0d2101aaf18
dev-d7 <167406376+dev-d7@users.noreply.github.com>
2024-04-26 11:23:45 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// # Primitive 
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Example 
+
+// JS is a dynamic language or Static Language ??
+// -> Varibale type 
+
+const score  = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const id  = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid)
+
+const bigNumber = 123456789n
+
+console.log(typeof bigNumber);
+
+
+// # Reference -> Non Primitve data type
+
+// Array, Object, Functions
+
+const hero = ["shaktimaan", "naagraj", "doga"];
+
+let myobj ={
+    name: "Devansh",
+    age: "23"
+}
+
+
+
+
+
+
+
+
